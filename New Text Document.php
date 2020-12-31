1f2b94c558ab98db644343680f0ff6f65c441a79
mo-adel007 <67648966+mo-adel007@users.noreply.github.com>
2020-12-31 17:28:19 +0200

    Add files via upload

---
+++ New Text Document.php
@@ -0,0 +1,46 @@
+<head><link rel="stylesheet" href="css/style.css" /></head>
+   
+
+    <div class="portfolio-content">
+      <div class="portfolio-upper row">
+        <div class="col-md-20 overlay-relative ">
+          <img src="img/p-1.jpg" class=" img-fluid" alt="">
+          <div class="over-lay">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+          </div>
+        </div>
+        <div class="col-md-4 overlay-relative ">
+          <img src="img/p-2.jpg" alt="">
+          <div class="over-lay">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+          </div>
+        </div>
+        <div class="col-md-4 overlay-relative ">
+          <img src="img/p-3.jpg" alt="">
+          <div class="over-lay">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+          </div>
+        </div>
+      </div>
+      <div class="portfolio-lower row">
+        <div class="col-md-4 overlay-relative ">
+          <img src="img/p-4.jpg" alt="">
+          <div class="over-lay">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+          </div>
+        </div>
+        <div class="col-md-4 overlay-relative ">
+          <img src="img/p-5.jpg" alt="">
+          <div class="over-lay">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+          </div>
+        </div>
+        <div class="col-md-4 overlay-relative ">
+          <img src="img/p-6.jpg" alt="">
+          <div class="over-lay">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sass project</title>
+
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2"><a href="inde.html" class="logo" >SASS</a></div>
+                <div class="col-md-10">
+                    <div class="menu text-right">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </header>
+    <div class="main-content">
+      <div class="main-content-wrapper">
+        <h1 class="text-center">SASS</h1>
+        <h5 class="text-center">SASS is Awesome ! We can do our styles using it in nice way.</h5>
+      </div>
+    </div>
+    <div class="portfolio-section container">
+      <div class="portfolio-title">
+        <h1 class="text-center">Portfolio</h1>
+        <h5 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
+      </div>
+      <div class="portfolio-content">
+        <div class="portfolio-upper row">
+          <div class="col-md-4 overlay-relative ">
+            <img src="img/p-1.jpg" class=" img-fluid" alt="">
+            <div class="over-lay">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+            </div>
+          </div>
+          <div class="col-md-4 overlay-relative ">
+            <img src="img/p-2.jpg" alt="">
+            <div class="over-lay">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+            </div>
+          </div>
+          <div class="col-md-4 overlay-relative ">
+            <img src="img/p-3.jpg" alt="">
+            <div class="over-lay">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-lower row">
+          <div class="col-md-4 overlay-relative ">
+            <img src="img/p-4.jpg" alt="">
+            <div class="over-lay">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+            </div>
+          </div>
+          <div class="col-md-4 overlay-relative ">
+            <img src="img/p-5.jpg" alt="">
+            <div class="over-lay">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+            </div>
+          </div>
+          <div class="col-md-4 overlay-relative ">
+            <img src="img/p-6.jpg" alt="">
+            <div class="over-lay">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ullam quas nam at quisquam, magnam incidunt, adipisci molestias nobis a quibusdam sint blanditiis aut sed enim similique! Animi, maiores nam?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="contact-section">
+      <button>Contact Us</button>
+    </div>
+  </body>
+</html>
